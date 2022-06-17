3e2826a6d6dfe9157319cce5cbe2341ece3b7eb5
ValentinPg <valentinpugliesew@outlook.com>
2022-06-17 11:21:40 -0300

    Separacion de los datos en arrays para facilitar su conversion en tabla y agrego funcion para limpiar arrays

--- home.js
+++ home.js
@@ -1,23 +1,49 @@
-const tablaDolar = document.getElementById("promociones");
+const tablaDolar = document.getElementById("pruebas");
 const URLApi = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+let nombresDolar = [];
+let ventaDolar = [];
+let compraDolar = [];
 
-function precio_dolar(){
+//consigo la info de la api
+function conseguirDatos(){
+    
    let api = fetch(URLApi);
 
    let apiResponse = api.then(response => response.json());
 
-   let  datosTabla = apiResponse.then(function(data) { console.log(data);
+   let  datosTabla = apiResponse.then(function(data) { 
 
-        let filtro = data.map(x => {if (x.casa.nombre.includes("Dolar")){
+         data.map(x => {if (x.casa.nombre.includes("Dolar")){
 
-            return x.casa.nombre + " Compra= " + x.casa.compra + " Venta= " + x.casa.venta;
+         nombresDolar.push(x.casa.nombre);
+         ventaDolar.push(x.casa.venta);
+         compraDolar.push(x.casa.compra);
 
-        } else {
-            return x.casa.nombre + " Compra= " + x.casa.compra + " Venta= " + x.casa.venta;
-            
         }});
+        //Debug
+        console.log(ventaDolar);
+        console.log(nombresDolar);
+        console.log(compraDolar);
 
-        console.log(filtro);
     })
- 
 }
+
+
+//resetear datos arrays
+function borrarAray(){
+     nombresDolar = [];
+     ventaDolar = [];
+     compraDolar = [];
+
+}
+
+ //prueba de una lista que muestre los valores de venta de cada tipo de dolar
+function imprimirDolar(){
+    let lista = nombresDolar.forEach(element => {
+        let valores = document.createElement("li");
+        tablaDolar.appendChild(valores);
+        valores.innerHTML = element + "= " + ventaDolar[nombresDolar.indexOf(element)];
+    });
+    
+}
+
