9cad6d2cce2ce5d69f20d73b9c23c5bb019cc1b7
ValentinPg <valentinpugliesew@outlook.com>
2022-07-01 11:53:39 -0300

    Creo archivos python y csv

---
+++ python/batch-cheques.py
@@ -0,0 +1,12 @@
+# NroCheque: Número de cheque, este debe ser único por cuenta.
+# CodigoBanco: Código numérico del banco, entre 1 y 100.
+# CodigoScurusal: Código numérico de la sucursal del banco va entre 1 y 300.
+# NumeroCuentaOrigen: Cuenta de origen del cheque.
+# NumeroCuentaDestino: Cuenta donde se cobra el cheque.
+# Valor: float con el valor del cheque.
+# FechaOrigen: Fecha de emisión: (En timestamp)
+# FechaPago: Fecha de pago o cobro del cheque (En timestamp)
+# DNI: String con DNI del cliente donde se permite identificarlo
+# Estado: Puede tener 3 valores pendiente, aprobado o rechazado.
+# TIPO: "EMITIDO" O "DEPOSITADO"
+
