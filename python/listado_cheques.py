1a519a008188fbe0f032c2adf884251ea05f8dab
valentintubaro <valentintubaro@yahoo.com>
2022-07-02 17:08:54 -0300

    "cheques"

--- python/listado_cheques.py
+++ python/listado_cheques.py
@@ -10,6 +10,7 @@
 # Estado: Puede tener 3 valores pendiente, aprobado o rechazado.
 # TIPO: "EMITIDO" O "DEPOSITADO"
 
+from calendar import c
 import csv
 from logging.config import dictConfig
 from operator import index
@@ -19,7 +20,7 @@ print("Bienvenido a ITBANK, este es el sistema de busqueda de cheques")
 print('los datos se encuentran almacenados en el archivo "info cheques.csv"')
 
 dni = input("ingrese el DNI del cliente (sin puntos): ")
-
+estado = input("Ingrese el estado de cheque que desea conocer (ACEPTADO, RECHAZADO o PENDIENTE.): ")
 # salida = (input("ingrese si quiere que la salida sea en formato pantalla o CSV: ")).lower()
 
 # while salida != "pantalla" and salida != "csv":
@@ -41,25 +42,37 @@ def obtenerInfo():
                 print(f"el cheque fue {tipoCheque}")
                 print(f"el estado del cheque es: {estadoCheque}")
                 
-obtenerInfo()
+
 
 def estadoCheque():   
     with open("python\info-cheques.csv") as abrirArchivo:
         archivo = csv.DictReader(abrirArchivo)
         for linea in archivo:
-            if dni == linea["DNI"]:
+            if dni == linea["DNI"] and estado != linea["Estado"]:
+                print('No se encontraron coincidencias con el estado de cheque solicitado para el DNI correspondiente.')
+            if dni == linea["DNI"] and estado == linea["Estado"]:
                 estadoCheque = linea["Estado"]
+                nCheque = linea["NroCheque"]
+                cBanco = linea["CodigoBanco"]
+                cSucursal = linea["CodigoScurusal"]
+                nCO = linea["NumeroCuentaOrigen"]
+                NCD = linea["NumeroCuentaDestino"]
+                valor = linea["Valor"]
+                fechaOrigen = linea["FechaOrigen"]
+                FechaPago = linea["FechaPago"]
+                Tipo = linea["Tipo"]
+
                 
-              
-                if estadoCheque == estado:
-                    print(f'El numero de cheque es: ')
-                    print(f'El código de banco es: ')
-                    print(f'El código de sucursal es: ')
-                    print(f'El número de cuenta de origen es: ')
-                    print(f'El número de cuenta de destino es: ')
-                    print(f'El valor del cheque es: $')
-                    print(f'La fecha de origen del cheque es: ')
-                    print(f'La fecha de pago del cheque es: ')
-                    print(f'El tipo de cheque es: ')
-                else:
-                     print('No se encontraron coincidencias con el estado de cheque solicitado para el DNI correspondiente.')
+                print(f'El numero de cheque es: {nCheque} ')
+                print(f'El código de banco es: {cBanco} ')
+                print(f'El código de sucursal es: {cSucursal} ')
+                print(f'El número de cuenta de origen es: {nCO} ')
+                print(f'El número de cuenta de destino es: {NCD} ')
+                print(f'El valor del cheque es: ${valor} ')
+                print(f'La fecha de origen del cheque es: {fechaOrigen} ')
+                print(f'La fecha de pago del cheque es: {FechaPago} ')
+                print(f'El tipo de cheque es: {Tipo}')
+                break
+
+estadoCheque()
+#obtenerInfo()
