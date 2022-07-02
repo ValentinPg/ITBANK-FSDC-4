c597c9aa7e1b5140ef36d1f816c05c1d431130d6
VanAckereMA <vanackeremelina@gmail.com>
2022-07-02 17:59:14 -0300

    Agrego funcionalidad modificada

--- python/listado_cheques.py
+++ python/listado_cheques.py
@@ -10,54 +10,79 @@
 # Estado: Puede tener 3 valores pendiente, aprobado o rechazado.
 # TIPO: "EMITIDO" O "DEPOSITADO"
 
-from ast import Return
+from ast import Return, While
 import csv
 from logging.config import dictConfig
 from operator import index
 import os
 
-# print("Bienvenido a ITBANK, este es el sistema de busqueda de cheques")
-# print('los datos se encuentran almacenados en el archivo "info cheques.csv"')
+# # print("Bienvenido a ITBANK, este es el sistema de busqueda de cheques")
+# # print('los datos se encuentran almacenados en el archivo "info cheques.csv"')
 
-#funcion para evaluar el dni ingresado por el cliente
-def evaluar():
+# #funcion para evaluar el dni ingresado por el cliente
+# def evaluar():
+#     # Pedimos dni
+#     dni = input("Ingrese número de DNI del cliente ")
+#     # Evaluamos que el dni ingresado corresponde
+#     numbers = []
+#     while (len(dni) >= 8) and (len(dni) <= 10):
+#         for i in range(0, 10):
+#             numbers.append(str(i))
+#         for char in dni:
+#             if char not in numbers:
+#                 print("El dni es invalido, debe contener solo caracteres númericos")
+#         break
+#     return dni #Deberia retoranar el dni
+# evaluar()
+
+# #funcion para elegir formato de impresion
+# def formatoImpreso():
+#     # Damos a elegir dos opciones de formato de impresion "Pantalla" o "CSV"
+#     formato = input("¿Desea imprimir los datos en Pantalla o formato CSV? ").lower()
+#     if formato == "pantalla":
+#         formato_Pantalla = True
+#     else:
+#         formato_csv = True
+# formatoImpreso()
+
+# def pedirEstado():
+#     # Preguntamos si quiere visualizar el estado en que se encuentra el cheque
+#     estado = input("¿Desea visualizar el estado en que se encuentra los datos? ").lower()
+#     if estado == "si":
+#         visualizar_estado = True
+#     else:
+#         visualizar_estado = False    
+# pedirEstado()
+
+# def obtenerInfo():   
+#     with open("python\info-cheques.csv") as abrirArchivo:
+#         archivo = csv.DictReader(abrirArchivo)
+#         for linea in archivo:
+#             if dni in linea.values():
+#                 print(linea)
+# obtenerInfo()
+
+
+# Pedimos dni
+
+dni = input("Ingrese número de DNI del cliente ")
+
+def evaluar(numeroDni):
     # Pedimos dni
-    dni = input("Ingrese número de DNI del cliente ")
     # Evaluamos que el dni ingresado corresponde
     numbers = []
-    while (len(dni) >= 8) and (len(dni) <= 10):
+    while (len(numeroDni) >= 8) and (len(numeroDni) <= 10): # Evalua la longitud entre 8 y 10 caracteres
         for i in range(0, 10):
             numbers.append(str(i))
-        for char in dni:
+        for char in numeroDni:
             if char not in numbers:
                 print("El dni es invalido, debe contener solo caracteres númericos")
+                dni = input("Ingrese número de DNI del cliente ")
+                evaluar(dni)
         break
-    return dni #Deberia retoranar el dni
-evaluar()
+evaluar(dni)
 
-#funcion para elegir formato de impresion
-def formatoImpreso():
-    # Damos a elegir dos opciones de formato de impresion "Pantalla" o "CSV"
-    formato = input("¿Desea imprimir los datos en Pantalla o formato CSV? ").lower()
-    if formato == "pantalla":
-        formato_Pantalla = True
-    else:
-        formato_csv = True
-        #Deberia retornar el formato elegido en True
-formatoImpreso()
-
-#funcion para elegir visualizar el estado del cheque 
-def pedirEstado():
-    # Preguntamos si quiere visualizar el estado en que se encuentra el cheque
-    estado = input("¿Desea visualizar el estado en que se encuentra los datos? ").lower()
-    if estado == "si":
-        visualizar_estado = True
-    else:
-        visualizar_estado = False   
-        #Deberia retornar la opcion elegida en True o False 
-pedirEstado()
-
-def obtenerInfo():   
+def obtenerInfo():
     with open("python\info-cheques.csv") as abrirArchivo:
         archivo = csv.DictReader(abrirArchivo)
         for linea in archivo:
