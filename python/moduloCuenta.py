d45bf0ae6da67293568e68b97452dca52d0f8912
ValentinPg <valentinpugliesew@outlook.com>
2022-07-14 17:57:18 -0300

    subclase 1 semicompletada y subclase 2 completada

--- python/moduloCuenta.py
+++ python/moduloCuenta.py
@@ -6,7 +6,7 @@ from json.encoder import INFINITY
 
 
 class Cuenta(object):
-    def __init__(self,tipo_cliente,limite_extraccion_diario=0,limite_transferencia_recibida=0,monto=0,costo_transferencias=0,saldo_descubierto_disponible=0):
+    def __init__(self,tipo_cliente,saldo_en_caja=0,limite_extraccion_diario=0,limite_transferencia_recibida=0,monto=0,costo_transferencias=0,saldo_descubierto_disponible=0):
         try:
             self.limite_extraccion_diario = float(limite_extraccion_diario)
             self.limite_transferencia_recibida = float(limite_transferencia_recibida)
@@ -14,6 +14,7 @@ class Cuenta(object):
             self.costo_transferencias = float(costo_transferencias)
             self.saldo_descubierto_disponible = float(saldo_descubierto_disponible)
             self.tipo_cliente = tipo_cliente
+            self.saldo_en_caja = saldo_en_caja
             
             if self.tipo_cliente != "CLASSIC" and self.tipo_cliente != "GOLD" and self.tipo_cliente != "BLACK":
                 raise TypeError
@@ -22,19 +23,32 @@ class Cuenta(object):
             exit()
         except TypeError:
             print (f"El tipo de cliente '{self.tipo_cliente}' no existe")
+        
+        
+            #metodo para verificar el limite de extraccion de el individuo
+    def limiteExtraccion(self):  
+        pass
+    
+           
+    #metodo para verificar la recepcion del dinero       
+    def limiteRecepcion(self):
+        pass
+    
+    
+    #metodo que calcula el costo de la transferencia
+    def costoTransferencia(self):
+        pass
             
         
 #subclase 1    
 class Caja_ahorro_pesos(Cuenta):
-    def __init__(self, tipo_cliente, limite_extraccion_diario=0, limite_transferencia_recibida=0, monto=0, costo_transferencias=0, saldo_descubierto_disponible=0):
-        super().__init__(tipo_cliente, limite_extraccion_diario, limite_transferencia_recibida, monto, costo_transferencias, saldo_descubierto_disponible)
+    def __init__(self, tipo_cliente, saldo_en_caja=0, limite_extraccion_diario=0, limite_transferencia_recibida=0, monto=0, costo_transferencias=0, saldo_descubierto_disponible=0):
+        super().__init__(tipo_cliente, saldo_en_caja, limite_extraccion_diario, limite_transferencia_recibida, monto, costo_transferencias, saldo_descubierto_disponible)
         #la caja de ahorros no puede quedar en negativo
-        if self.monto >= self.saldo_descubierto_disponible:
+        if self.monto >= self.saldo_en_caja:
             print("no hay suficiente saldo disponible en la Caja de Ahorros para realizar la operacion")
             exit()
-    
-    
-    #metodo para verificar el limite de extraccion de el individuo
+            
     def limiteExtraccion(self):  
         #funcion auxiliar que me ayuda a calcular el limite de extraccion
         def prueboExtraccion(self):
@@ -56,8 +70,8 @@ class Caja_ahorro_pesos(Cuenta):
             self.limite_extraccion_diario = 100000
             prueboExtraccion(self)
                 #falta imprimir resultado en el HTML
-        self.saldo_descubierto_disponible = self.saldo_descubierto_disponible-self.monto-self.costoTransferencia()
-        return self.saldo_descubierto_disponible
+        self.saldo_en_caja = self.saldo_en_caja-self.monto-self.costoTransferencia()
+        return self.saldo_en_caja
     
            
     #metodo para verificar la recepcion del dinero       
@@ -80,8 +94,8 @@ class Caja_ahorro_pesos(Cuenta):
             self.limite_transferencia_recibida = self.monto + 1
             prueboRecepcion(self)
             #falta imprimir resultado en el HTML
-        self.saldo_descubierto_disponible = self.saldo_descubierto_disponible+self.monto-self.costoTransferencia()
-        return self.saldo_descubierto_disponible
+        self.saldo_en_caja = self.saldo_en_caja+self.monto-self.costoTransferencia()
+        return self.saldo_en_caja
     
     
     #metodo que calcula el costo de la transferencia
@@ -98,20 +112,46 @@ class Caja_ahorro_pesos(Cuenta):
         return costoDebitado
     
     def __str__(self) -> str:
-        return f"La caja de ahorro tiene un saldo de ${self.saldo_descubierto_disponible}"
-            
-        
+        return f"La caja de ahorro tiene un saldo de ${self.saldo_en_caja}"
+    
+    
+
 #Subclase 2
 class Caja_ahorro_dolares(Cuenta):
-    pass
+    def limiteExtraccion(self):
+        print("No se pueden hacer transferencias en dolares")
+    
+    def limiteRecepcion(self):
+        print("No se pueden hacer transferencias en dolares")
+    
+    def comprarDolar(self):
+        if self.tipo_cliente == "BLACK" or self.tipo_cliente == "GOLD":
+            self.saldo_en_caja = self.saldo_en_caja+self.monto
+        else:
+            print(f"el tipo de cliente {self.tipo_cliente} no tiene caja en dolares")
+
 #Subclase 3
 class Cuenta_corriente(Cuenta):
-    pass
+    def __init__(self, tipo_cliente, saldo_en_caja=0, limite_extraccion_diario=0, limite_transferencia_recibida=0, monto=0, costo_transferencias=0, saldo_descubierto_disponible=0):
+        super().__init__(tipo_cliente, saldo_en_caja, limite_extraccion_diario, limite_transferencia_recibida, monto, costo_transferencias, saldo_descubierto_disponible)
+        if self.tipo_cliente == "GOLD" or self.tipo_cliente == "BLACK":
+            saldo_descubierto_disponible = 10000
+    
+    def limiteExtraccion(self):
+        super().limiteExtraccion()
+        if self.tipo_cliente == "CLASSIC":
+            print("este tipo de cliente no tiene habilitada una cuenta corriente")
+    
+    def limiteRecepcion(self):
+        super().limiteRecepcion()
+        if self.tipo_cliente == "CLASSIC":
+            print("este tipo de cliente no tiene habilitada una cuenta corriente")
+    
 
 
 
 
 #pRUEBa
-xa = Caja_ahorro_pesos(tipo_cliente="CLASSIC", monto=20001, saldo_descubierto_disponible=100000)
-xa = xa.limiteExtraccion()
+xa = Caja_ahorro_pesos(tipo_cliente="CLASSIC", monto=2000, saldo_en_caja=100000)
+xa = xa.limiteRecepcion()
 print(xa)
