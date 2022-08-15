52aa62172244e9dd1c5266f572171eb5fcbea755
ValentinPg <valentinpugliesew@outlook.com>
2022-08-15 13:42:30 -0300

    modelo de solicitud

--- Django/homebanking/homebank/models.py
+++ Django/homebanking/homebank/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SolicitudesPrestamos(models.Model):
+    id_solicitud = models.AutoField(primary_key=True)
+    monto = models.CharField(max_length=100,verbose_name="monto")
+    fecha_inicio =  models.DateField(verbose_name="fecha de inicio")
+    tipo_prestamo = models.TextField(max_length=100,default="tipo_prestamo", verbose_name="tipo de prestamo")
+    
+    fecha_solicitud = models.DateTimeField(auto_now_add=True, null=True, verbose_name="fecha de creación")
+    fecha_modificacion = models.DateTimeField(auto_now_add=True, null=True, verbose_name="fecha de modificación")
+    
+    # user_id = models.models.ForeignKey(, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.id_solicitud
