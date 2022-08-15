95477cd0ecb52ba0fdf060615cf5b1a3d59842ea
ValentinPg <valentinpugliesew@outlook.com>
2022-08-15 15:39:48 -0300

    Vincualacion forms y base de datos

--- Django/homebanking/homebank/models.py
+++ Django/homebanking/homebank/models.py
@@ -1,16 +1,20 @@
+
+from urllib import request
 from django.db import models
+
+from clientes.models import User
 
 
 class SolicitudesPrestamos(models.Model):
+    
     id_solicitud = models.AutoField(primary_key=True)
     monto = models.CharField(max_length=100,verbose_name="monto")
     fecha_inicio =  models.DateField(verbose_name="fecha de inicio")
-    tipo_prestamo = models.TextField(max_length=100,default="tipo_prestamo", verbose_name="tipo de prestamo")
+    tipo_prestamo = models.CharField(max_length=100,default="tipo_prestamo", verbose_name="tipo de prestamo")
     
     fecha_solicitud = models.DateTimeField(auto_now_add=True, null=True, verbose_name="fecha de creación")
     fecha_modificacion = models.DateTimeField(auto_now_add=True, null=True, verbose_name="fecha de modificación")
     
-    # user_id = models.models.ForeignKey(, on_delete=models.CASCADE)
     
-    def __str__(self) -> str:
-        return self.id_solicitud
+    usuario = models.IntegerField()
+    
--- Django/homebanking/homebank/views.py
+++ Django/homebanking/homebank/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from clientes.models import User
+
+from homebank.models import SolicitudesPrestamos
 
 from .forms import SolicitudPrestamo
 
@@ -11,6 +14,7 @@ def vista(request):
 @login_required
 def formularioSolicitud(request):
     formulario_solicitud = SolicitudPrestamo
+    current_user = request.user  #obtengo el usuario loggeado
     
     if request.method == "POST":
         formulario_solicitud = formulario_solicitud(data=request.POST)
@@ -20,6 +24,9 @@ def formularioSolicitud(request):
             fecha_inicio = request.POST.get("fecha_inicio", "")
             tipo_prestamo = request.POST.get("tipo_prestamo", "")
             
+            entry = SolicitudesPrestamos(monto=monto, fecha_inicio=fecha_inicio, tipo_prestamo=tipo_prestamo, usuario=current_user.customer_id)
+            entry.save()
+                      
         return redirect(reverse('prestamos')+ '?OK')
     return render(request,"homebank/prestamos.html", {"form":formulario_solicitud})
 
