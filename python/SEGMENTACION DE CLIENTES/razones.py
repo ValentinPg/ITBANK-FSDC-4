d97b901c39e5e9a6b60c7d79291903e9e63731c2
ValentinPg <valentinpugliesew@outlook.com>
2022-09-01 18:11:28 -0300

    Mensajes de error mas completos

--- python/SEGMENTACION DE CLIENTES/razones.py
+++ python/SEGMENTACION DE CLIENTES/razones.py
@@ -1,12 +1,3 @@
-from ast import Return
-from copy import copy
-from http import client
-from mimetypes import init
-from pydoc import apropos
-from moduloCuenta import Cuenta_corriente,Caja_ahorro,Caja_dolares
-from direccion import Direccion
-from JSONprueba import Json,eventos_black,eventos_classic,eventos_gold
-
 
 class Razon(object):
     def __init__(self,type = None) -> None:
@@ -27,7 +18,7 @@ class RazonAltaChequera(Razon):
             if key["tipo"] == "ALTA_CHEQUERA":
                 try:
                     if key["totalChequerasActualmente"] >= self.cliente.maxChequera:
-                        key["razon"] = "ha superado el limte de chequeras"
+                        key["razon"] = f"ha superado el limte de chequeras de su cuenta, el limite es {self.cliente.maxChequera}"
                         RazonAltaChequera.rechazados.append(key)
                     else:
                         RazonAltaChequera.aprobados.append(key)
@@ -51,7 +42,7 @@ class RazonAltaTarjetaCredito(Razon):
             if key["tipo"] == "ALTA_TARJETA_CREDITO":
                 try:
                     if key["totalTarjetasDeCreditoActualmente"] >= self.cliente.maxCredito:
-                        key["razon"] = "ha superado el limte de tarjetas de credito"
+                        key["razon"] = f"ha superado el limte de tarjetas de credito, el limite de esta cuenta es {self.cliente.maxCredito}"
                         RazonAltaTarjetaCredito.rechazados.append(key)
                     else:
                         RazonAltaTarjetaCredito.aprobados.append(key)
@@ -70,7 +61,7 @@ class RazonCompraDolar(Razon):
         for key in self.cliente.transacciones:
             if key["tipo"] == "COMPRA_DOLAR":
                 if cliente.puede_comprar_dolar() is False :
-                    key["razon"] = "cliente no habilitado para comprar dolares"
+                    key["razon"] = "cliente no habilitado para comprar dolares, debe ser BLACK o GOLD"
                     RazonCompraDolar.rechazados.append(key)
                 elif key["monto"] > key["saldoEnCuenta"]:
                     key["razon"] = "Limite de saldo alcanzado"
@@ -91,7 +82,7 @@ class RazonRetiroEfectivo(Razon):
         for key in self.cliente.transacciones:
             if key["tipo"] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
                 if key["cupoDiarioRestante"] < key["monto"]:
-                    key["razon"] = "Cupo diario de extraccion superado"
+                    key["razon"] = f"Cupo diario de extraccion superado, no puede superar los ${key['cupoDiarioRestante']} por dia"
                     RazonRetiroEfectivo.rechazados.append(key)
                 elif key["saldoEnCuenta"] <= key["monto"]:
                     key["razon"] = "Saldo insuficiente"
@@ -114,7 +105,7 @@ class RazonTransferenciaEnviada(Razon):
             if key["tipo"] == "TRANSFERENCIA_ENVIADA":
                 if cliente.cuenta_corriente:
                     if (key["monto"]+(self.cliente.caja_ahorro.costo_transferencias*key["monto"])) > key["saldoEnCuenta"]:
-                        key["razon"] = f"En las cuentas {self.cliente.tipo} el saldo no puede quedar negativo"
+                        key["razon"] = f"Saldo insuficiente, en las cuentas {self.cliente.tipo} el saldo no puede quedar negativo"
                         RazonTransferenciaEnviada.rechazados.append(key)
                     else:
                         RazonTransferenciaEnviada.aprobados.append(key)
@@ -136,7 +127,7 @@ class RazonTransferenciaRecibida(Razon):
         for key in self.cliente.transacciones:
             if key["tipo"] == "TRANSFERENCIA_RECIBIDA":
                 if key["monto"] > self.cliente.caja_ahorro.limite_transferencia_recibida and self.cliente.caja_ahorro.limite_transferencia_recibida != 0:
-                    key["razon"] = "Limite de transferecia recibida excedidio, debe pedir autorizacion"
+                    key["razon"] = "Limite de transferecia recibida excedido, debe pedir autorizacion al banco para realizar transferencias tan grandes"
                     RazonTransferenciaRecibida.rechazados.append(key)
                 else:
                     RazonTransferenciaRecibida.aprobados.append(key)
