fc0e9e8a64291ba30cd46eb717a80243efc84f59
LSMZ23 <103401571+LSMZ23@users.noreply.github.com>
2022-09-02 16:53:15 -0300

    Se organizo la generacion del html en print_HTML.py Se modificaron los mensajes de algunas razones en razones.py

--- python/SEGMENTACION DE CLIENTES/print_HTML.py
+++ python/SEGMENTACION DE CLIENTES/print_HTML.py
@@ -27,8 +27,36 @@ def obtenerRazones(cliente):
 
 def conversion(lista):
     for x in enumerate(lista):
+        print(x)
         with open("python\SEGMENTACION DE CLIENTES\indexpy.html", "a+") as e:
-            e.write (f"""<p>{x}</p>\n""")
+            if x[1]['estado'] == 'RECHAZADA':
+                if x[1]['tipo'] == 'ALTA_CHEQUERA':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']} chequeras actuales: {x[1]['totalChequerasActualmente']}</p>\n""")
+                elif x[1]['tipo'] == 'ALTA_TARJETA_CREDITO':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']} tarjetas actuales: {x[1]['totalTarjetasDeCreditoActualmente']}</p>\n""")
+                elif x[1]['tipo'] == 'RETIRO_EFECTIVO_CAJERO_AUTOMATICO':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']}: Cupo diario restante: {x[1]['cupoDiarioRestante']} | monto solicitado: {x[1]['monto']} </p>\n""")
+                elif x[1]['tipo'] == 'COMPRA_DOLAR':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']}</p>""")
+                elif x[1]["tipo"] == "TRANSFERENCIA_ENVIADA":
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']} | saldo restante: {x[1]['monto'] - x[1]['saldoEnCuenta']}</p>\n""")
+                elif x[1]["tipo"] == "TRANSFERENCIA_RECIBIDA":
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | motivo: {x[1]['razon']} | monto de transferencia: {x[1]['monto']}</p>\n""")
+
+            elif x[1]['estado'] == 'ACEPTADA':
+                if x[1]['tipo'] == 'ALTA_CHEQUERA':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | chequeras actuales: {x[1]['totalChequerasActualmente']}</p>\n""")
+                elif x[1]['tipo'] == 'ALTA_TARJETA_CREDITO':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | tarjetas actuales: {x[1]['totalTarjetasDeCreditoActualmente']}</p>\n""")
+                elif x[1]['tipo'] == 'RETIRO_EFECTIVO_CAJERO_AUTOMATICO':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | Cupo diario restante: {x[1]['cupoDiarioRestante']} | monto solicitado: {x[1]['monto']} </p>\n""")
+                elif x[1]['tipo'] == 'COMPRA_DOLAR':
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | </p>""")
+                elif x[1]["tipo"] == "TRANSFERENCIA_ENVIADA":
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | saldo restante: {x[1]['monto'] - x[1]['saldoEnCuenta']}</p>\n""")
+                elif x[1]["tipo"] == "TRANSFERENCIA_RECIBIDA":
+                    e.write (f"""<p>{x[0]} | {x[1]['fecha']} | {x[1]['tipo']} | CUENTA NUMERO {x[1]['cuentaNumero']} | {x[1]['estado']} | monto de transferencia: {x[1]['monto']}</p>\n""")
+                    
 
 def plantillaHTML(cliente):
     archivoHTML = open("python\SEGMENTACION DE CLIENTES\indexpy.html", "w")
