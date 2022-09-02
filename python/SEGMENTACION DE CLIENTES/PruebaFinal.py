f4808cf87780d0901afb439c060f29651dd5eb5b
ValentinPg <valentinpugliesew@outlook.com>
2022-09-02 13:12:30 -0300

    limpieza modulos

--- python/SEGMENTACION DE CLIENTES/PruebaFinal.py
+++ python/SEGMENTACION DE CLIENTES/PruebaFinal.py
@@ -1,15 +1,12 @@
-from JSONprueba import eventos_black,eventos_classic,eventos_gold, iniciarPrograma
-from moduloCliente import Cliente_black,Cliente_clasico,Cliente_gold
+from JSONprueba import iniciarPrograma
 from print_HTML import plantillaHTML
-from JSONprueba import Json
 
 
 
-#-------------------------Ingrese el archivo que se desea escanear (eventos_classic,eventos_gold,eventos_black)---------------------------------------------------------#
+
+#-------------------------Ingrese el archivo que se desea escanear (classic,gold,black)---------------------------------------------------------#
 archivo = input("Ingrese el archivo que desea leer: ")
-
-
 
 x = iniciarPrograma(archivo)
 plantillaHTML(x)
-#--------------------------El archivo HTML se llama: indexpy.html (Caprpeta "SEGMENTACION CLIENTES"-------------------------------------------------------------------------------------------------------#
+#--------------------------El archivo HTML se llama: indexpy.html (Carpeta "SEGMENTACION CLIENTES")-------------------------------------------------------------------------------------------------------#
